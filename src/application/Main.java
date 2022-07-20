71f8f1be5b1514443352eaae28568aed369686af
AlvaroX017 <alvarosousa722@gmail.com>
2022-07-19 23:40:51 -0300

    Checkmate logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main {
 		
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -51,6 +51,8 @@ public class Main {
 				keyboard.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
